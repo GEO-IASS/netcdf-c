8a1abd7a8136bc05e47501598e715b70e3ead594
Dennis Heimbigner <dmh@ucar.edu>
2017-02-01 12:15:49 -0700

    ckp

--- dap4_test/test_test.sh
+++ dap4_test/test_test.sh
@@ -2,6 +2,8 @@
 
 if test "x$srcdir" = "x" ; then srcdir=`dirname $0`; fi; export srcdir
 
-sh -x ${srcdir}/test_parse.sh >& ./parse.log >& /dev/tty
+rm -f log.log
+sh -x ${srcdir}/test_parse.sh >& ./parse.log >& log.log
+cat log.log /dev/tty
 
 exit 0
